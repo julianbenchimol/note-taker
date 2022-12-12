85b03364b0d1008c74cd0edd2f8b7a5d71a456ca
Julian Benchimol <jbenchimol1999@gmail.com>
2022-12-12 10:17:02 -0500

    Fixxed issue with index.html not being served

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //HTML Routes
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 })
 
 app.get('/notes', (req, res)=>{
